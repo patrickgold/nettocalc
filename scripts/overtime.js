f5810364041de50f27d6cc935c96fe077c90a562
Philipp Trummler <philipp.trummler@gmx.at>
2020-12-22 08:13:16 +0100

    Reuploaded oldchanges for Overtime.js after some problems

--- scripts/overtime.js
+++ scripts/overtime.js
@@ -13,49 +13,105 @@ class Overtime{
     }*/
 
     clcOvertime() {
+        this.output.print("OcOvertimeHourlyLoan", 0, "number")
+        this.output.print("OcOvertimeBaseLoan", 0, "number")
+        this.output.print("OcOvertimeAddition", 0, "number")
+        this.output.print("OcOvertimeFirst10", 0, "number")
+        this.output.print("OcOvertimeOther", 0, "number")
+        this.output.print("OcOvertimeTotal", 0, "number")
+
+        this.output.print("OcBruttoAB", 0, "number")
+        this.output.print("OcOvertimeLoan", 0, "number")
+        this.output.print("OcSvDna", 0, "number")
+        this.output.print("OcIncomeTax10h", 0, "number")
+        this.output.print("OcAssassmentBasis", 0, "number")
+        this.output.print("OcIncomeTaxRate", 0, "percent")
+        this.output.print("OcIncomeTaxBeforeAvab", 0, "number")
+        this.output.print("OcAvab", 0, "number")
+        this.output.print("OcIncomeTaxAfterAvab", 0, "number")
+        this.output.print("BruttoAb2", 0, "number")
+        this.output.print("OcOvertimeLoan2", 0, "number")
+        this.output.print("OcSvDna2", 0, "number")
+        this.output.print("OcIncomeTax", 0, "number")
+        this.output.print("OcNetto", 0, "number")
+
+
+
         if(this.input.bruttoTypeHourly.checked == true){
-            var Ot10H = 0
-            var OtOthers = 0
-            var hours = 0
-            var brutto = 0
-            var UeZ = 0
-            var rest = 0
-            var divSum = 0
-            var Sum = 0
+            var overtime40 = 0
+            var hours = parseInt(this.input.hours.value) + parseInt(this.input.overtimeHours.value)
+            var addition = 0
+            var tmp = 0
+            var Ot10h = 0
+            var Otothers = 0
+            var overtimeTotal = 0
 
             if(this.input.hasOvertime.checked == true){
-                brutto = this.input.hourlyRate.value * this.input.hours.value
-                hours = this.input.hours.value + this.input.overtimeHours.value
-                divSum = this.input.overtimeDivider.value == 0 ? brutto/158 : brutto/this.input.overtimeDivider.value
-                UeZ = brutto * 0.5
-                if(hours > 40){
-                    rest = hours - 40
-                    if(rest > 10) {
-                        Ot10H = 10 * UeZ
-                        OtOthers = (rest - 10) * UeZ
+                // Overtime hourly loan
+                this.output.print("OcOvertimeHourlyLoan", this.input.hourlyRate.value, "number")
+
+                // Overtime base loan
+                if(hours > 40) {
+                    tmp = hours - 40
+                    overtime40 = tmp * this.input.hourlyRate.value
+                    this.output.print("OcOvertimeBaseLoan", overtime40, "number")
+
+                    // Overtime addition
+                    addition = parseInt(this.input.hourlyRate.value) * 0.5
+                    this.output.print("OcOvertimeAddition", addition, "number")
+
+                    // Overtime money 10h & others
+                    if(tmp > 10) {
+                        var tmp10 = 10
+                        var tmpO = 0
+                        tmpO = tmp - tmp10
+                        Ot10h = tmp10 * addition
+                        Otothers = tmpO * addition
+                        this.output.print("OcOvertimeFirst10", Ot10h, "number")
+                        this.output.print("OcOvertimeOther", Otothers, "number")
+                        
+                    }else {
+                        Ot10h = addition * tmp
+                        Otothers = 0
+                        this.output.print("OcOvertimeFirst10", Ot10h, "number")
+                        this.output.print("OcOvertimeOther", Otothers, "number")
                     }
-                    else {
-                        Ot10H = rest * UeZ
-                        OtOthers = 0
-                    }
-                    Sum = brutto + OtOthers + Ot10H
+                    overtimeTotal = overtime40 + Ot10h + Otothers
+                    this.output.print("OcOvertimeTotal", overtimeTotal, "number")
+                    
                 }
-                else{
-                    Sum = brutto
-                }
-                this.output.OcOvertimeTotal.innerHTML = Sum
-                console.log(Sum)
+            }
+            else{
+                this.output.print("OcOvertimeHourlyLoan", 0, "number")
+                this.output.print("OcOvertimeBaseLoan", 0, "number")
+                this.output.print("OcOvertimeAddition", 0, "number")
+                this.output.print("OcOvertimeFirst10", 0, "number")
+                this.output.print("OcOvertimeOther", 0, "number")
+                this.output.print("OcOvertimeTotal", 0, "number")
+                this.output.print("OcBruttoAB", 0, "number")
+                this.output.print("OcOvertimeLoan", 0, "number")
+                this.output.print("OcSvDna", 0, "number")
+                this.output.print("OcIncomeTax10h", 0, "number")
+                this.output.print("OcAssassmentBasis", 0, "number")
+                this.output.print("OcIncomeTaxRate", 0, "percent")
+                this.output.print("OcIncomeTaxBeforeAvab", 0, "number")
+                this.output.print("OcAvab", 0, "number")
+                this.output.print("OcIncomeTaxAfterAvab", 0, "number")
+                this.output.print("BruttoAb2", 0, "number")
+                this.output.print("OcOvertimeLoan2", 0, "number")
+                this.output.print("OcSvDna2", 0, "number")
+                this.output.print("OcIncomeTax", 0, "number")
+                this.output.print("OcNetto", 0, "number")
             }
         }
         if(this.input.bruttoTypeGiven.checked == true) {
-            /*
-            this.output.Overtime10h.innerHTML =  0
-            this.output.OvertimeOther.innerHTML = 0
-            this.output.overtimeTotal.innerHTML = 0
-            */
+
         }
     }
 
+    /*
+    * print -> this.output.print("<idName>", <variable>, "<number; percent>")
+    */
 
     /*
     * ÜGL
