a6ae827047052d989cd77ed2334e4e8e81a7a2b0
Philipp Trummler <philipp.trummler@gmx.at>
2021-01-04 13:34:52 +0100

    Created clcHolidayPay() function in holiday_pay.js

--- scripts/holiday_pay.js
+++ scripts/holiday_pay.js
@@ -3,4 +3,22 @@ class Holiday{
         this.christmas_bonus = christmas == true ? (amount - allowance) : 0;
         this.vecation_pay = vecation == true ? (amount-allowance) : 0;
     };
+
+    clcHolidayPay(){
+        /*
+        * Urlaubsgeld
+        * - Gew
+        * - PendlerPauschal
+        * - E-Card
+        * __________________
+        * LST-BMGL
+        * %         Money
+        * - AVAB
+        * - P€
+        */
+       if(this.input.hasVacationPay.checked == true){
+           var hol_brutto = this.input.vacationPay.value * 1
+           
+       }
+    }
 }
